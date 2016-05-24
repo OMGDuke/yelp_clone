8b8de2959bd8bb6a224db557afcc848528e815e7
Hanna <hannaeberhoefer@yahoo.com>
2016-05-24 11:34:18 +0100

    Can add new restaurant

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 feature 'restaurants' do
+
   context 'no restaurants have been added' do
     scenario 'should display a prompt to add a restaurant' do
       visit '/restaurants'
@@ -10,14 +11,26 @@ feature 'restaurants' do
   end
 
   context 'restaurants have been added' do
+
     before do
       Restaurant.create(name: 'KFC')
     end
 
-    scenario 'display restaurants' do
+    scenario 'should display restaurants' do
       visit '/restaurants'
       expect(page).to have_content('KFC')
-      expecy(page).not_to have_content('No restaurants yet')
+      expect(page).not_to have_content('No restaurants yet')
+     end
+   end
+
+   context 'creating restaurants' do
+     scenario 'prompts user to fill out a form, then displays the new restaurant' do
+       visit '/restaurants'
+       click_link 'Add a restaurant'
+       fill_in 'Name', with: 'KFC'
+       click_button 'Create Restaurant'
+       expect(page).to have_content 'KFC'
+       expect(current_path).to eq '/restaurants'
      end
    end
 end
